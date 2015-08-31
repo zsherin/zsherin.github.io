c61d5144534bcb13b129587f9ece709e6eb0187d
Zach Sherin <zarrro45@gmail.com>
2015-08-30 19:29:54 -0700

    Content for all the pages except mine! Hoorah.

--- index.js
+++ index.js
@@ -24,8 +24,8 @@
 					"The Virtual Mars Project uses an Oculus Rift and a Vicon motion tracking system to place the user on Mars and let them walk around the planet. The virtual experience was found to be far better for mission planning, collaboration and understanding of Martian terrain than the previously used panorama method.",
 					"This dome, using over 90 1-Watt LEDs, flashes special patterns of light onto an object, and is able to use the photos it collects to produce a texture for 3D rendering. It essentially takes a real world object, and lets you use it in your virtual environment.",
 					"Built at the same lab as the Virtual Mars Project, Sidekick uses Microsoft's Hololens to aid astronauts on the International Space Station with their daily tasks.",
-					"This software allows the user to determine whether or not an object that they want to build is stable. Given a 3D model and the material properties, the system will spit out a new, stable model that is similar to the given model.",
-					"The VrMFA is a daily constructed museum, using art drawn from the website DeviantArt. It is an attempt to allow anyone with a phone to access a museum experience, and a rebuke of the classical standards that determines what art appears in museums.",];
+					"This software allows the user to find a model that will come to rest in a desired pose, if made from a certain material. Given a 3D model and the material properties, the system will spit out a new model that will deform under gravity into the correct shape.",
+					"The vrmfa is a daily constructed virtual museum using art drawn from the website DeviantArt. It's built for use with the Google Cardboard, to lower the barrier to entry for virtual reality. It is an attempt to allow anyone with a phone to access a museum experience, and a rebuke of the classical standards that determines what art appears in museums.",];
 		$(".texter").on('transitionend webkitTransitionEnd', function(e){
         // Callback function
 			console.log("hey");
@@ -40,15 +40,17 @@
 			//$("#stretch-img").attr('src',"./Images/stretch2.png");
 			//$("#stretch-img").animate({height: 400, width: 250});
 			
-				$(".texter").text(texts[$(this).attr("index")]);
-				$(".texter").css({'line-height':'25px'});
-				$(".texter").css({'color':'#3A5897'});
-				
-				$(".text-header").text(titles[$(this).attr("index")]);
-				$(".text-header").css({'line-height':'25px'});
-				$(".text-header").css({'color':'#3A5897'});
-				console.log("moving");
+			$(".texter").text(texts[$(this).attr("index")]);
+			$(".texter").css({'line-height':'35px'});
+			$(".texter").css({'color':'#3A5897'});
 			
+			$(".text-header").text(titles[$(this).attr("index")]);
+			$(".text-header").css({'line-height':'35px'});
+			$(".text-header").css({'color':'#3A5897'});
+			/*$('html, body').animate({
+				scrollTop: $(document).height()
+				}, 'slow');
+			*/
 		});
 		/*$(".square").mouseleave(function()
 		{
